8ca298bc25d83fe9a2ad00b261b1ddc1a8668e78
Sachin Das <sachindas437@gmail.com>
2026-05-24 13:29:15 +0530

    Application version updated along with deployment yaml version update.

--- app/server.js
+++ app/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const APP_VERSION = process.env.APP_VERSION || "1.0.0";
+const APP_VERSION = process.env.APP_VERSION || "2.0.0";
 const APP_ENV = process.env.APP_ENV || "development";
 
 app.get('/', (req, res) => {
-    res.send('Cloud-Native DevOps Platform on AWS');
+    res.send('Project: Cloud-Native DevOps Platform on AWS');
 });
 
 app.get('/health', (req, res) => {
