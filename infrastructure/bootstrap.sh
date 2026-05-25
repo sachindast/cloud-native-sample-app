29b94435f482ea4eacb189eec671084efbe3356b
Sachin Das <sachindas437@gmail.com>
2026-05-25 17:02:23 +0000

    Add Kubernetes bootstrap automation

---
+++ infrastructure/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+echo "Creating kind cluster"
+
+kind create cluster --config infrastructure/kind-config.yaml
+
+echo "Installing ingress controller"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+echo "Waiting for ingress controller"
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo "Bootstrap completed"
